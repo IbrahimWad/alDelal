606ac5831886d2b17261688d59dbde97cef5c175
IbrahimWad <ibrahim.amude.781@gmail.com>
2024-04-18 22:27:53 +0300

    Get houses

--- lib/core/viewModel/home_viewmodel.dart
+++ lib/core/viewModel/home_viewmodel.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
+import 'package:aldlal/view/widget/storag_constant.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
 
 class HomeViewModel extends GetxController {
   late final RxInt _currentFocusType = 1.obs;
@@ -9,5 +14,41 @@ class HomeViewModel extends GetxController {
     _currentFocusType.value = value;
 
     update();
+  }
+
+  getHouses() async {
+    try {
+      var response = await http.get(Uri.parse(''), headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ${StoragConstant.token}',
+      });
+
+      var responseBody = jsonDecode(response.body);
+
+      if (responseBody['status'] == false) {
+        Get.snackbar('خطـــأ', 'الخطأ: ${responseBody['error']}',
+            colorText: Colors.white,
+            duration: Duration(seconds: 20),
+            snackPosition: SnackPosition.BOTTOM);
+      }
+
+      if (currentFocusType == 1) {
+        /// الحصول على المعلومات هنا
+      }
+      if (currentFocusType == 2) {
+        /// الحصول على المعلومات هنا
+      }
+      if (currentFocusType == 3) {
+        /// الحصول على المعلومات هنا
+      }
+    } catch (e) {
+      Get.snackbar('خطـــأ', 'خطــأ في تحميل المعومات',
+          colorText: Colors.white,
+          duration: Duration(seconds: 20),
+          snackPosition: SnackPosition.BOTTOM);
+
+      print(e);
+    }
   }
 }
