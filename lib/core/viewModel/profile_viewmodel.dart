7f9b8eae55fbfefc5a8b9c0cc86ef31f0579e551
IbrahimWad <ibrahim.amude.781@gmail.com>
2024-05-01 20:50:07 +0300

    Delete account

--- lib/core/viewModel/profile_viewmodel.dart
+++ lib/core/viewModel/profile_viewmodel.dart
@@ -2,9 +2,13 @@ import 'package:aldlal/core/service/show_diloag_alret.dart';
 import 'package:aldlal/main.dart';
 import 'package:aldlal/view/auth/auth.dart';
 import 'package:aldlal/view/home/control_view.dart';
+import 'package:aldlal/view/widget/color_constant.dart';
+import 'package:aldlal/view/widget/custom_button.dart';
+import 'package:aldlal/view/widget/custom_text.dart';
 import 'package:aldlal/view/widget/storag_constant.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
 
 class ProfileViewModel extends GetxController {
   logOut() {
@@ -36,14 +40,99 @@ class ProfileViewModel extends GetxController {
     );
   }
 
-  delete() {
+  delete() async {
     ShowDiloagAlretService().showDiloagAlret(
         height: 200,
         onPressed2: () {
           Get.back();
         },
-        onPressed: () {
+        onPressed: () async {
+          if (box.read(StoragConstant.token)) {
+            Get.snackbar('خطـــأ', 'انت غير مسجل',
+                colorText: Colors.white,
+                duration: Duration(seconds: 20),
+                snackPosition: SnackPosition.BOTTOM);
+          }
+
           // make request here
+          var response = await http.delete(Uri.parse(''), headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ${box.read(StoragConstant.token)}',
+          });
+
+          if (response.statusCode == 200) {
+            box.erase();
+            Get.back();
+            Get.dialog(Center(
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  color: ColorConstant.backgroundColor,
+                ),
+                height: 180,
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 50,
+                    ),
+                    CustomText(
+                        text: 'تم حذف حسابك',
+                        alignment: Alignment.center,
+                        color: ColorConstant.warning,
+                        fontSize: 24,
+                        fontWeight: FontWeight.w400),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    CustomButton(
+                      onPressed: () {
+                        Get.back();
+                      },
+                      text: 'الرجوع',
+                      color: ColorConstant.secondTextColor,
+                      buttonColor: ColorConstant.textColor,
+                    )
+                  ],
+                ),
+              ),
+            ));
+          } else {
+            Get.back();
+            Get.dialog(Center(
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  color: ColorConstant.backgroundColor,
+                ),
+                height: 180,
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 50,
+                    ),
+                    CustomText(
+                        text: 'لا يمكن حذف هذا الحساب',
+                        alignment: Alignment.center,
+                        color: ColorConstant.warning,
+                        fontSize: 24,
+                        fontWeight: FontWeight.w400),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    CustomButton(
+                      onPressed: () {
+                        Get.back();
+                      },
+                      text: 'الرجوع',
+                      color: ColorConstant.secondTextColor,
+                      buttonColor: ColorConstant.textColor,
+                    )
+                  ],
+                ),
+              ),
+            ));
+          }
         },
         text2: 'الرجوع',
         text: 'حذف الحساب',
