d3eb6cd73904472e196647dadd381c3c27d7a207
IbrahimWad <ibrahim.amude.781@gmail.com>
2024-05-05 22:35:10 +0300

    Work on search bar

--- lib/view/search/search_view.dart
+++ lib/view/search/search_view.dart
@@ -2,6 +2,7 @@ import 'package:aldlal/core/viewModel/search_viewmodel.dart';
 import 'package:aldlal/view/widget/color_constant.dart';
 import 'package:aldlal/view/widget/custom_text.dart';
 import 'package:aldlal/view/widget/grid_view_container.dart';
+import 'package:app_bar_with_search_switch/app_bar_with_search_switch.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -15,39 +16,41 @@ class SearchView extends StatelessWidget {
       builder: (controller) {
         return Scaffold(
           backgroundColor: ColorConstant.backgroundColor,
-          appBar: AppBar(
-            backgroundColor: ColorConstant.backgroundColor,
-            elevation: 0,
-            leading: IconButton(
-              icon: Image.asset('assets/images/search.png', height: 25),
-              onPressed: () {
-                showSearch(context: context, delegate: controller);
-              },
-            ),
-            actions: [
-              Container(
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    CustomText(
-                      text: 'الرجوع',
-                      fontSize: 20,
-                      alignment: Alignment.center,
-                    ),
-                    IconButton(
-                      onPressed: () {
-                        Get.back();
-                      },
-                      icon: Icon(
-                        Icons.arrow_forward_ios_outlined,
-                        size: 20,
-                        color: ColorConstant.textColor,
+          appBar: AppBarWithSearchSwitch(
+            onChanged: (text) {
+              // here I will change the search
+            }, // onSubmitted
+            appBarBuilder: (context) => AppBar(
+              backgroundColor: ColorConstant.backgroundColor,
+              elevation: 0,
+              iconTheme:
+                  const IconThemeData(color: ColorConstant.textColor, size: 30),
+              leading: const AppBarSearchButton(),
+              actions: [
+                Container(
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      CustomText(
+                        text: 'الرجوع',
+                        fontSize: 20,
+                        alignment: Alignment.center,
                       ),
-                    ),
-                  ],
-                ),
-              )
-            ],
+                      IconButton(
+                        onPressed: () {
+                          Get.back();
+                        },
+                        icon: Icon(
+                          Icons.arrow_forward_ios_outlined,
+                          size: 20,
+                          color: ColorConstant.textColor,
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
           body: Container(
             child: Column(
